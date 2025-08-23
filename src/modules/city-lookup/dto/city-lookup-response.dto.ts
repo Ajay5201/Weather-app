71574f9c63d7f9ec69adf48cdaf126fb95f8c5ea
ajay <ajay123mohan@gmail.com>
2025-08-24 02:09:32 +0530

    integrated geo apify api

---
+++ src/modules/city-lookup/dto/city-lookup-response.dto.ts
@@ -0,0 +1,22 @@
+// dto/city-search-result.dto.ts
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CitySearchResultDto {
+  @ApiProperty({ example: 'Coimbatore' })
+  name: string;
+
+  @ApiProperty({ example: 'IN' })
+  country: string;
+
+  @ApiProperty({ example: 'Tamil Nadu', required: false })
+  state?: string;
+
+  @ApiProperty({ example: 11.0168 })
+  latitude: number;
+
+  @ApiProperty({ example: 76.9558 })
+  longitude: number;
+
+  @ApiProperty({ example: 'Coimbatore, Tamil Nadu, IN' })
+  displayName: string;
+}
